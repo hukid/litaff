88adc2950c4f4c3692526207acf15c1c873de4aa
Xiao Hu <xiaoh@microsoft.com>
2017-10-28 15:35:04 -0700

    Ethereal

--- server/backend/utils/getEmailTransporter.js
+++ server/backend/utils/getEmailTransporter.js
@@ -1,17 +1,27 @@
 const nodemailer = require('nodemailer');
 
 module.exports = () => {
-  const sender = 'seattletruelight@outlook.com';
+  const sender = 'ycxw6cd767fqcfon@ethereal.email';
   const transporter = nodemailer.createTransport({
-    host: 'smtp-mail.outlook.com',
-    port: '587',
+    host: 'smtp.ethereal.email',
+    port: 587,
     auth: {
-      user: sender,
-      pass: 'YR>Ff1]f2Lwa',
+      user: 'ycxw6cd767fqcfon@ethereal.email',
+      pass: 'nrxAMvtxMcJfrtJXtq',
     },
-    secureConnection: true,
-    tls: { ciphers: 'SSLv3' },
   });
+
+  // const sender = 'seattletruelight@outlook.com';
+  // const transporter = nodemailer.createTransport({
+  //   host: 'smtp-mail.outlook.com',
+  //   port: '587',
+  //   auth: {
+  //     user: sender,
+  //     pass: 'YR>Ff1]f2Lwa',
+  //   },
+  //   secureConnection: true,
+  //   tls: { ciphers: 'SSLv3' },
+  // });
 
   transporter.sender = sender;
   return transporter;
