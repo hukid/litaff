ddb251bb400f9a18f7ab4afb10d1777c8cea8820
hukid <hukid23@gmail.com>
2017-10-28 23:39:53 -0700

    Enable email in Azure (#6)
    * Ethereal
    
    * revert back to outlook
    
    * use sendgrid
    
    * change to truelight_seattle

--- server/backend/utils/getEmailTransporter.js
+++ server/backend/utils/getEmailTransporter.js
@@ -1,17 +1,37 @@
 const nodemailer = require('nodemailer');
 
 module.exports = () => {
-  const sender = 'seattletruelight@outlook.com';
+  // const sender = 'ycxw6cd767fqcfon@ethereal.email';
+  // const transporter = nodemailer.createTransport({
+  //   host: 'smtp.ethereal.email',
+  //   port: 587,
+  //   auth: {
+  //     user: 'ycxw6cd767fqcfon@ethereal.email',
+  //     pass: 'nrxAMvtxMcJfrtJXtq',
+  //   },
+  // });
+
+  const sender = 'truelight_seattle@outlook.com';
   const transporter = nodemailer.createTransport({
-    host: 'smtp-mail.outlook.com',
+    host: 'smtp.sendgrid.net',
     port: '587',
     auth: {
-      user: sender,
-      pass: 'YR>Ff1]f2Lwa',
+      user: 'apikey',
+      pass: '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',
     },
-    secureConnection: true,
-    tls: { ciphers: 'SSLv3' },
+    secure: false,
   });
+
+  // const sender = 'seattletruelight@outlook.com';
+  // const transporter = nodemailer.createTransport({
+  //   host: 'smtp-mail.outlook.com',
+  //   port: '587',
+  //   auth: {
+  //     user: sender,
+  //     pass: 'YR>Ff1]f2Lwa',
+  //   },
+  //   secure: false,
+  // });
 
   transporter.sender = sender;
   return transporter;
