d8dd045f444e5ce3eebeeb20dc760f1d54246949
Xiao Hu <xiaoh@microsoft.com>
2017-10-28 22:44:57 -0700

    use sendgrid

--- server/backend/utils/getEmailTransporter.js
+++ server/backend/utils/getEmailTransporter.js
@@ -13,14 +13,25 @@ module.exports = () => {
 
   const sender = 'seattletruelight@outlook.com';
   const transporter = nodemailer.createTransport({
-    host: 'smtp-mail.outlook.com',
+    host: 'smtp.sendgrid.net',
     port: '587',
     auth: {
-      user: sender,
-      pass: 'YR>Ff1]f2Lwa',
+      user: 'apikey',
+      pass: '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',
     },
     secure: false,
   });
+
+  // const sender = 'seattletruelight@outlook.com';
+  // const transporter = nodemailer.createTransport({
+  //   host: 'smtp-mail.outlook.com',
+  //   port: '587',
+  //   auth: {
+  //     user: sender,
+  //     pass: 'YR>Ff1]f2Lwa',
+  //   },
+  //   secure: false,
+  // });
 
   transporter.sender = sender;
   return transporter;
